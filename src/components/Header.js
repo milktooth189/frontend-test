42d119668aa3dcea7bb7e88b41029e23aed6aab1
milktooth189 <arrogantpanty@gmail.com>
2024-03-15 13:01:53 +0700

    done header +40mins

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,66 @@
 import React, {useEffect, useState} from "react"
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
   const [info, setInfo] = useState(0);
+  const [menuIsOpen, setMenuOpen] = useState(false);
+
+  const menuItemList = ["自分の記録", "体重グラフ", "目標", "選択中のコース", "コラム一覧", "設定"];
 
   useEffect(() => {
     setInterval(() => setInfo(Math.floor(Math.random() * 5) + 1), 10000)
+
   }, []);
 
   return (
-    <div className="header">
-      <div className="header-logo">
-        <img src="/icon/logo.png" alt="Logo"/>
+    <>
+      <div className="header">
+        <div className="header-logo">
+          <img src="/icon/logo.png" alt="Logo"/>
+        </div>
+        <NavLink to={"/memo"} className="header-nav">
+          <div className="header-nav-icon">
+            <img src="/icon/icon_memo.png" alt="Memo"/>
+          </div>
+          <div className="header-nav-text">
+            自分の記録
+          </div>
+        </NavLink>
+        <NavLink to={"/challenge"} className="header-nav">
+          <div className="header-nav-icon">
+            <img src="/icon/icon_challenge.png" alt="Chalenge"/>
+          </div>
+          <div className="header-nav-text">
+            チャレンジ
+          </div>
+        </NavLink>
+        <NavLink to={"/info"} onClick={() => setInfo(0)} className="header-nav">
+          <div className="header-nav-icon">
+            <img src="/icon/icon_info.png" alt="Info"/>
+            {info > 0 && <span className="notification">{info}</span>}
+          </div>
+          <div className="header-nav-text">
+            お知らせ
+          </div>
+        </NavLink>
+        <div className="header-menu">
+          <img onClick={() => setMenuOpen(!menuIsOpen)}
+               src={menuIsOpen ? "/icon/icon_close.png" : "/icon/icon_menu.png"}
+               alt="Menu"/>
+        </div>
       </div>
-      <div className="header-nav">
-        <div className="header-nav-icon">
-          <img src="/icon/icon_memo.png" alt="Memo"/>
-        </div>
-        <div className="header-nav-text">
-          自分の記録
-        </div>
-      </div>
-      <div className="header-nav">
-        <div className="header-nav-icon">
-          <img src="/icon/icon_challenge.png" alt="Chalenge"/>
-        </div>
-        <div className="header-nav-text">
-          チャレンジ
-        </div>
-      </div>
-      <div onClick={() => setInfo(0)} className="header-nav">
-        <div className="header-nav-icon">
-          <img src="/icon/icon_info.png" alt="Info"/>
-          {info > 0 && <span className="notification">{info}</span>}
-        </div>
-        <div className="header-nav-text">
-          お知らせ
-        </div>
-      </div>
-      <div className="header-menu">
-        <img src="/icon/icon_menu.png" alt="Menu"/>
-      </div>
-    </div>
+      {<div className="menu">
+        {menuItemList.map((item, index) => (
+          <div key={`menu-item-${index}`} className={"menu-item" + (menuIsOpen ? " show" : "")}
+               style={{
+                 transition: `opacity ${(menuIsOpen ? index : (menuItemList.length - index)) * 0.2}s ease, transform 1s ease`,
+                 cursor: menuIsOpen ? "pointer" : "default"
+               }}>
+            {item}
+          </div>
+        ))}
+      </div>}
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom/client"
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
-} from "react-router-dom";import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+import {store, persistor} from "./store";
 
-import './index.scss';
+import "./index.scss";
 
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />}>
-            <Route path="dashboard" element={<App />} />
-        </Route>
-    )
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route path="dashboard" element={<App/>}/>
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                <RouterProvider router={router} />
-            </PersistGate>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <RouterProvider router={router}/>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
