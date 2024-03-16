85fdbe01e4de9afe06453d23b805242509c1a92c
milktooth189 <arrogantpanty@gmail.com>
2024-03-16 07:56:47 +0700

    done diary +30mins

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,20 @@ import {Outlet} from "react-router-dom";
 
 const App = (props) => {
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth" // hoặc 'auto' nếu bạn muốn scroll mà không muốn hiệu ứng smooth
+    });
+  };
+
   return (
     <div className="container">
       <Header
       />
       <Outlet/>
       <Footer/>
+      <img className="scroll-top" onClick={scrollToTop} src="/icon/component_scroll.png" alt="scroll"/>
     </div>
   )
 }
--- src/components/my-record/MyRecord.js
+++ src/components/my-record/MyRecord.js
@@ -27,6 +27,16 @@ const MyRecord = (props) => {
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
+  const diaryArr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  const diaryItem = {
+    date: "2021.05.21",
+    time: "23:25",
+    title: "私の日記の記録が一部表示されます。",
+    content: "私の日記の記録が一部表示されます。\n" +
+      "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…",
+  }
+
   return (
     <div className={"memo"}>
       <div className="memo-action">
@@ -115,6 +125,26 @@ const MyRecord = (props) => {
             ))}
           </div>
         </PerfectScrollbar>
+      </div>
+      <div className="diary">
+        <div className="diary-title">MY DIARY</div>
+        <div className="diary-content">
+          {diaryArr.map((item, index) => (
+            <div key={`diary-item-${index}`} className="diary-content-item">
+              <div className="diary-content-item-time">
+                <span>{diaryItem.date}</span>
+                <span>{diaryItem.time}</span>
+              </div>
+              <div className="diary-content-item-content">{diaryItem.content}</div>
+            </div>
+          ))}
+
+        </div>
+      </div>
+      <div className="add-daily">
+        <div className="add-daily-btn">
+          <span>自分の日記をもっと見る</span>
+        </div>
       </div>
     </div>
   )
