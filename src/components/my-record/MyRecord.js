ccd56c2e122ce93a80d63921720852b8f2b15aa1
milktooth189 <arrogantpanty@gmail.com>
2024-03-16 16:09:32 +0700

    add helpers and constant dir

--- src/components/my-record/MyRecord.js
+++ src/components/my-record/MyRecord.js
@@ -4,6 +4,7 @@ import "react-perfect-scrollbar/dist/css/styles.css";
 import PerfectScrollbar from "react-perfect-scrollbar"
 import Header from "../Header";
 import Footer from "../Footer";
+import {diaryItem, fakeArr, diaryArr, exeItem} from "../../constants/constants";
 
 const MyRecord = (props) => {
   const [currentTime, setCurrentTime] = useState("");
@@ -20,25 +21,6 @@ const MyRecord = (props) => {
     return () => clearInterval(interval);
   }, []);
 
-  const exeItem = {
-    text: "家事全般（立位・軽い）",
-    cal: "26kcal",
-    time: "10 mins",
-  }
-
-  const fakeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-
-  const diaryArr = [1, 2, 3, 4, 5, 6, 7, 8];
-
-  const diaryItem = {
-    date: "2021.05.21",
-    time: "23:25",
-    title: "私の日記の記録が一部表示されます。",
-    content: "私の日記の記録が一部表示されます。\n" +
-      "テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト…",
-  }
 
   //handle scroll to top
   const scrollToTop = () => {
